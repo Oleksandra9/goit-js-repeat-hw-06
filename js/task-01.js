7c577f08a0b5c665c37ecc8570412cae16d1bc0a
Oleksandra9 <sashakrytska@gmail.com>
2023-02-23 11:33:09 +0200

    +

---
+++ js/task-01.js
@@ -0,0 +1,75 @@
+
+// const C = {
+//     d: 6,
+//     v:9,
+// }
+// const D = C
+
+// console.log(C)
+
+// C.l = 4;
+
+
+
+// console.log(D)
+
+
+
+
+
+// function A () {
+//     // console.log("Im body of function")
+//     return "Im return of function A"
+
+// }
+
+// const B = A();
+
+
+
+
+
+// const navEl = document.querySelector(".nav-list")
+// const magicBtn = document.querySelector(".hello")
+
+// console.log(magicBtn)
+
+
+// magicBtn.addEventListener("click", () => {
+//     magicBtn.classList.toggle("hello-change")
+// })
+
+
+
+
+// const titleEl = document.createElement("h1")
+// titleEl.classList.add("page-title")
+// titleEl.textContent = "Im page title"
+
+// console.log(titleEl)
+
+// document.body.appendChild(titleEl)
+
+// const navElement = document.createElement("li")
+// navElement.classList(".new-element")
+
+
+
+
+const books = [
+    {
+      title: "The Last Kingdom",
+      author: "Bernard Cornwell",
+      rating: 8.38,
+    },
+    {
+      title: "На березі спокійних вод",
+      author: "Роберт Шеклі",
+      rating: 8.51,
+    },
+    {
+      title: "Сон смішної людини",
+      author: "Федір Достоєвський",
+      rating: 7.75,
+    },
+  ];
